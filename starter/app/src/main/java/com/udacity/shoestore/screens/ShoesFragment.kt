1a4127e638ffbf9d7f8976470d961b8e7e0c2224
never934 <48915853+never934@users.noreply.github.com>
2021-02-18 14:00:36 +0300

    shoes fragment + item. Fragments init

---
+++ starter/app/src/main/java/com/udacity/shoestore/screens/ShoesFragment.kt
@@ -0,0 +1,27 @@
+package com.udacity.shoestore.screens
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.LoginFragmentBinding
+import com.udacity.shoestore.databinding.ShoesFragmentBinding
+
+class ShoesFragment : Fragment() {
+
+    private lateinit var binding: ShoesFragmentBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View {
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.shoes_fragment,
+            container,
+            false
+        )
+        return binding.root
+    }
+}
